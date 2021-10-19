58e9b9950e506d6fe68ea010aefeddd53808b810
kylian-2612 <71694721+kylian-2612@users.noreply.github.com>
2021-10-19 12:58:54 +0200

    fixed technical test

--- technical_test/js/script.js
+++ technical_test/js/script.js
@@ -1,10 +1,19 @@
-fetch('https://goweather.herokuapp.com/weather/Brussels')
-    .then(response => response.json())
-    .then(test => {
-        let data = test
-        console.log(data);
-        let weather = `temperature: ${test.temperature} description:${test.description} `;
-        document.getElementById('testWindow').innerHTML = weather;
+let url = 'http://api.openweathermap.org/data/2.5/weather?q=denderleeuw&appid=d3cdfff8a9c9b9f89b250cc4b6bc8e18';
 
-    });
-    
+
+console.log("heye")
+async function getData(url) {
+
+    let response = await fetch(url);
+
+    return await response.json();
+
+}
+
+getData(url).then(movie => {
+
+    console.log('movie');
+
+    document.getElementById('testWindow').innerText = JSON.stringify(movie);
+
+});
